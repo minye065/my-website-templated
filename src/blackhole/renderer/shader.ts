451a6e7e633311ee54d9abb7ee9bbf37ac48f7fe
minye065 <minye065@fusdk12.net>
2026-09-04 22:26:03 -0700

    v3.14 blackhole tuning

--- src/blackhole/renderer/shader.ts
+++ src/blackhole/renderer/shader.ts
@@ -182,7 +182,8 @@ float sky(vec3 rd)
 
 void main()
 {
-    vec2 uv = ((gl_FragCoord.xy - 0.5 * resolution) / resolution.y) + 0.6;
+    vec2 uv = ((gl_FragCoord.xy - 0.5 * resolution) / resolution.y);
+    uv -= vec2(0.4, 0.6)
 
     float ci = cos(inclination), si = sin(inclination);
     float ca = cos(azimuth), sa = sin(azimuth);
