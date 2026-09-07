d5524542f7ecb4eb698ac2208dbc26581dec7dc2
minye065 <minye065@fusdk12.net>
2026-09-07 14:57:02 -0700

    in/out threshold 30->80

--- src/blackhole/renderer/mask.ts
+++ src/blackhole/renderer/mask.ts
@@ -36,7 +36,7 @@ export function returnMask(pixelData:Uint8Array, canvasWidth:number, canvasHeigh
                 if(mask[neighborY * maskWidth + neighborX] === 0)
                 {
                     let currentPixel = (neighborY * canvasWidth + neighborX) * step * 4;
-                    if(pixelData[currentPixel] + pixelData[currentPixel + 1]  + pixelData[currentPixel + 2] <= 30)
+                    if(pixelData[currentPixel] + pixelData[currentPixel + 1]  + pixelData[currentPixel + 2] <= 80)
                     {
                         mask[neighborY * maskWidth + neighborX] = pixelStatus.PIXEL_DARK;
                         queue.push([neighborX, neighborY]);
