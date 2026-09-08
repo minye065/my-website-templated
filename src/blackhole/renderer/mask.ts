e2c3da7a46d5aabd44be275e45ebda31b9f01d54
minye065 <minye065@fusdk12.net>
2026-09-07 19:45:06 -0700

    v3.2.8 900-> 100

--- src/blackhole/renderer/mask.ts
+++ src/blackhole/renderer/mask.ts
@@ -36,7 +36,7 @@ export function returnMask(pixelData:Uint8Array, canvasWidth:number, canvasHeigh
                 if(mask[neighborY * maskWidth + neighborX] === 0)
                 {
                     let currentPixel = ((neighborY * step) * canvasWidth + (neighborX * step)) * 4;
-                    if(pixelData[currentPixel] + pixelData[currentPixel + 1]  + pixelData[currentPixel + 2] <= 900)
+                    if(pixelData[currentPixel] + pixelData[currentPixel + 1]  + pixelData[currentPixel + 2] <= 100)
                     {
                         mask[neighborY * maskWidth + neighborX] = pixelStatus.PIXEL_DARK;
                         queue.push([neighborX, neighborY]);
